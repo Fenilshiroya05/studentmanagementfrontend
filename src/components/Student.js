e98d3336444c1ae11ca40e1ae89567f0f9860311
20it143fenilshiroya <20it143@charusat.edu.in>
2022-09-04 20:33:03 +0530

    Added Navigationbar

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, {useState} from "react";
 import { Container, Form, Card, Button } from "react-bootstrap";
-import CardHeader from "react-bootstrap/esm/CardHeader";
 
 
 export default function Student() {
@@ -41,26 +40,19 @@ export default function Student() {
     <div  className="my-3">
     <Container>
       <Card>
-        <Form>
         <Form onSubmit={saveStudent}>
           <Card.Header>Add Student Information</Card.Header>
           <Card.Body>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Group className="mb-3">
               <Form.Label>Id</Form.Label>
-              <Form.Control type="text" placeholder="Enter email" />
               <Form.Control name="id" value={id} type="text" placeholder="Enter id" onChange={textChanged}/>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter email" />
               <Form.Control name="name" value={name} type="text" placeholder="Enter name" onChange={textChanged} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Group className="mb-3">
               <Form.Label>Address</Form.Label>
-              <Form.Control type="text" placeholder="Enter email" />
               <Form.Control name="address" value={address} type="text" placeholder="Enter address" onChange={textChanged} />
             </Form.Group>
           </Card.Body>
