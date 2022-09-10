9c4bfb8d6799aca7893608e45c5d4dc62c160b30
20it143fenilshiroya <20it143@charusat.edu.in>
2022-09-10 09:58:46 +0530

    Added Delete student functionality

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -3,14 +3,11 @@ import { Card, Container, Table, ButtonGroup, Button } from "react-bootstrap";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
-
-export default function StudentList(props) {
+export default function StudentList() {
   const [students, setStudents] = useState([]);
-
   useEffect(() => {
     getStudents();
   }, []);
-
   let getStudents = () => {
     axios
       .get("http://localhost:8080/listStudents")
@@ -18,7 +15,15 @@ export default function StudentList(props) {
       .catch((error) => alert(error));
   };
 
+  let deleteStudent = (studentId) => {
+    axios.delete("http://localhost:8080/student/"+studentId)
+    .then(response=> {
+      if (response.data !== null){
+        alert("Record Deleted Successfully");
 
+      }
+    })
+  }
 
   return (
     <div className="my-3">
@@ -47,14 +52,14 @@ export default function StudentList(props) {
                   <td>{student.id}</td>
                   <td>{student.name}</td>
                   <td>{student.address}</td>
-                  
                   <td>
                     <ButtonGroup>
                       <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
                       <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                      <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this,student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                      {/* <Button size="sm" variant="outline-danger" onClick={()=>deleteStudent(student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button> */}
                     </ButtonGroup>
                   </td>
-                  
                 </tr>
                 )
               )}
